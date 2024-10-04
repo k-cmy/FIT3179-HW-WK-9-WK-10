57d7ef11233a838ec9515f67efb2d720c84f5fc6
Chung Min Yi <you@example.com>
2024-10-04 21:55:24 +0800

    added js

---
+++ vega_lite_vis.js
@@ -0,0 +1,9 @@
+var vg_1 = "Js/MAP1.json";
+var vg_2 = "Js/plot.json";
+
+vegaEmbed("#choropleth_map", vg_1).then(function(result) {
+  
+}).catch(console.error);
+
+vegaEmbed("#plot", vg_2).then(function(result) {
+}).catch(console.error);
